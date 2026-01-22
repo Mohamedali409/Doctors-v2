bb52e3e9a651a940f53b175ad078e74d246f6e10
Mohamed <109993226+Mohamedali409@users.noreply.github.com>
2026-01-22 18:31:43 +0200

    Refactor MongoDB connection logic

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,33 +1,33 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
-
-  await mongoose.connect(`${process.env.MONGODB_URL}/prescripto`);
-};
-
-export default connectDB;
-
 // import mongoose from "mongoose";
 
-// let isConnected = false;
-
 // const connectDB = async () => {
-//   if (isConnected) {
-//     return;
-//   }
+//   mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-//   try {
-//     const db = await mongoose.connect(process.env.MONGODB_URL, {
-//       dbName: "prescripto",
-//     });
-
-//     isConnected = db.connections[0].readyState === 1;
-//     console.log("MongoDB Connected");
-//   } catch (error) {
-//     console.error("MongoDB Connection Error:", error);
-//     throw error;
-//   }
+//   await mongoose.connect(`${process.env.MONGODB_URL}/prescripto`);
 // };
 
 // export default connectDB;
+
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "prescripto",
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    throw error;
+  }
+};
+
+export default connectDB;
